33bcae5f3fa9e38a4c6259bf00e21ddb3a046928
Calvin Flegal <calvin.flegal@gmail.com>
2012-05-23 13:58:01 -0400

    Added Readme, fixed fileformats

---
+++ README.markdown
@@ -0,0 +1,4 @@
+INFO
+========
+This was a little script I wrote for [R2Integrated](http://www.r2integrated.com/). Using Pony and a csv file of contacts, we emailed all contacts a party invitation.
+I used an external file for the html, although I had manually entered the plain text content.
--- letter.rb
+++ letter.rb
@@ -1,16 +1,17 @@
 require 'Pony'
 htmlsource = IO.readlines("html.txt")
-#used txt for contact list due to change of line-ending encodings from CR to LF
-contactlist = IO.readlines("finallist.txt")
+contactlist = IO.readlines("finallist.csv")
 contactlist.map! do |item|
   #seperate the csv
   item.chomp.split(',')
 end
+
 subjectstring = %Q{Subjectline}
+
 for i in 0..contactlist.length-1
   Pony.mail({
     :to => "#{contactlist[i][1]}",
-    :from => 'amaddocks@r2integrated.com',
+    :from => 'from_email_address',
     :subject => subjectstring,
     :html_body => "Hi #{contactlist[i][0]}," + htmlsource.join,
     :content_type => 'text/plain',
